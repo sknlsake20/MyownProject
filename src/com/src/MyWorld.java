215c5ae5aa66e0ead0fa669a033944388a270c4b
Mahalakshmi.P.M <mahalakshmipmurugesan@gmail.com>
2021-09-02 17:46:09 +0530

    "hi hello"

--- src/com/src/MyWorld.java
+++ src/com/src/MyWorld.java
@@ -4,7 +4,7 @@ public class MyWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my world for next year");
+		System.out.println("Hi This is Mahalakshmi's world for Every year");
 	}
 
 }
