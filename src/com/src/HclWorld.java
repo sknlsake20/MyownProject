754c8fad7e7899754355031765ba21e9b119d805
swathielangovan <swathielango23@gmail.com>
2021-09-02 13:04:00 +0530

    Next Commit

---
+++ src/com/src/HclWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.src;
+
+/**
+ * @author admin
+ *
+ */
+public class HclWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi this is Hcl World");	}
+
+}
