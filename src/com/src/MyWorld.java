5907d9602bc4d327d13618cb171c86cd5869d3a7
navyask00 <navyask00@gmail.com>
2021-09-03 08:19:15 +0530

    Developer Commit

--- src/com/src/MyWorld.java
+++ src/com/src/MyWorld.java
@@ -4,7 +4,7 @@ public class MyWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my world for next 1000 years");
+		System.out.println("this is my world for next 100 years");
 	}
 
 }
