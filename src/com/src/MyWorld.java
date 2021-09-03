56a78cd9d112689548125018efd51a78ee85762d
POOJAV99 <89783083+POOJAV99@users.noreply.github.com>
2021-09-03 10:31:29 +0530

    message

--- src/com/src/MyWorld.java
+++ src/com/src/MyWorld.java
@@ -4,7 +4,7 @@ public class MyWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my world for every year");
+		System.out.println("this is my world for one year");
 	}
 
 }
